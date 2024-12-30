a5e777413844f90261b89d58829f3446aa557c77
Carlos Sandoval <carlosmsand22@yahoo.com>
2024-12-30 13:36:18 -0700

    created login page

---
+++ fantasy-app/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<h1>Fantasy Football</h1>
+<h3>Login</h3>    
+<form action="/login" method="POST">
+    <label for="username">Username:</label>
+    <input type="text" id="username" name="username" required />
+    
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password" required />
+    
+    <button type="submit">Login</button>
+</form>
